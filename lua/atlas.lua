b2b45cc0b46a1c669cb722e957e401c171b9ca1e
FesaGemini <fesagemini@gmail.com>
2022-03-14 11:46:53 +0700

    added mapfile function

--- lua/atlas.lua
+++ lua/atlas.lua
@@ -3,8 +3,11 @@ local atlas = {}
 function atlas.setup()
   vim.cmd[[
     command! Plugfile :e ~/.config/nvim/lua/core/plugins.lua
+  ]]
+  vim.cmd[[
     command! Mapfile :e ~/.config/nvim/lua/core/mappings.lua
   ]]
+
 end
 
 return atlas 
