c2089401956fabc4886c976e152a1c4810d88510
FesaGemini <fesagemini@gmail.com>
2022-03-14 11:42:52 +0700

    docs

--- lua/atlas.lua
+++ lua/atlas.lua
@@ -3,6 +3,7 @@ local atlas = {}
 function atlas.setup()
   vim.cmd[[
     command! Plugfile :e ~/.config/nvim/lua/core/plugins.lua
+    command! Mapfile :e ~/.config/nvim/lua/core/mappings.lua
   ]]
 end
 
